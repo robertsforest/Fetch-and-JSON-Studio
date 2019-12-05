cf8c7b269eea6bbfbbbcd9a73d1e308c38877a18
seldonatreides <seldonatreides@gmail.com>
2019-12-04 18:26:32 -0600

    Sorted descending by hours, added formatting and number of astronauts

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ window.addEventListener("load",function(event){
         response.json().then(function(json) {
             const div = document.getElementById("container");
             let currentAstronautBio;
+            json = json.sort(function(a,b){
+                return b.hoursInSpace - a.hoursInSpace;
+            });
             for(let i = 0; i < json.length; i++){
                 currentAstronautBio = json[i];
                 currentAstronautSkills = currentAstronautBio.skills.join(", ");
@@ -14,7 +17,7 @@ window.addEventListener("load",function(event){
                         <h3>${currentAstronautBio.firstName} ${currentAstronautBio.lastName}</h3>
                         <ul>
                             <li>${currentAstronautBio.hoursInSpace}</li>
-                            <li>Active: ${currentAstronautBio.active}</li>
+                            <li id="active${currentAstronautBio.active}">Active: ${currentAstronautBio.active}</li>
                             <li>Skills: ${currentAstronautSkills}</li>
                         </ul>
                         </div>
@@ -22,6 +25,11 @@ window.addEventListener("load",function(event){
                     </div>
                 `;
             }
+            div.innerHTML += `
+            <div id="astronautCount">
+                <h2>There are ${json.length} astronauts in this program.</h2>
+            </div>
+            `;
         });
     });
 });
